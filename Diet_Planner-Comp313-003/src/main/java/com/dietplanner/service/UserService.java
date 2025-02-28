36bb27a9dccad70c583451eeb9261a6bb908f899
Stephanie Santos <ssanto43@my.centennialcollege.ca>
2025-02-28 18:17:12 -0500

    Reset branch & commit user story files. Updated userController for account type. Updated UserService to add user validation for user exists. Improved registration page UI in register.html Changed accountType to accounttype in register.html to match in user.java and to post the account type properly in the account table.

--- Diet_Planner-Comp313-003/src/main/java/com/dietplanner/service/UserService.java
+++ Diet_Planner-Comp313-003/src/main/java/com/dietplanner/service/UserService.java
@@ -29,13 +29,26 @@ public class UserService implements UserDetailsService {
                 .roles(user.getAccounttype()) // Assign roles
                 .build();
     }
-
+    
     public User saveUser(User user) {
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            throw new IllegalArgumentException("User already exists");
+        }
+
         if (!user.getPassword().startsWith("$argon2id$")) { // Ensure password is hashed
             user.setPassword(passwordEncoder.encode(user.getPassword()));
         }
+
         return userRepository.save(user);
     }
+
+//  comment  
+  //  public User saveUser(User user) {
+//        if (!user.getPassword().startsWith("$argon2id$")) { // Ensure password is hashed
+//            user.setPassword(passwordEncoder.encode(user.getPassword()));
+//        }
+//        return userRepository.save(user);
+//    }
 
     public User findByUsername(String username) {
         return userRepository.findByUsername(username)
