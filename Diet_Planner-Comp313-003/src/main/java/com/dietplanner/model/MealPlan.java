f7e749dec4a48a3f89bc43b28e9711018a4cdd35
Aleli Macapagal <amacapag@my.centennialcollege.ca>
2025-03-05 11:31:05 -0500

    Fixed error in View Meal Plans after merging

--- Diet_Planner-Comp313-003/src/main/java/com/dietplanner/model/MealPlan.java
+++ Diet_Planner-Comp313-003/src/main/java/com/dietplanner/model/MealPlan.java
@@ -20,4 +20,42 @@ public class MealPlan {
     private Integer mealQuantity;
     private String assignedDay;
     
+    private int totalCalories;
+    private int totalCarbohydrate;
+    private int totalProtein;
+    private int totalFat;
+
+    
+    public int getTotalCalories() {
+        return totalCalories;
+    }
+
+    public int getTotalCarbohydrate() {
+        return totalCarbohydrate;
+    }
+
+    public int getTotalProtein() {
+        return totalProtein;
+    }
+
+    public int getTotalFat() {
+        return totalFat;
+    }
+    
+    public void setTotalCalories(Integer totalCalories) {
+        this.calories = totalCalories;
+    }
+
+    public void setTotalCarbohydrate(Integer totalCarbohydrate) {
+        this.carbohydrate = totalCarbohydrate;
+    }
+
+    public void setTotalProtein(Integer totalProtein) {
+        this.protein = totalProtein;
+    }
+
+    public void setTotalFat(Integer totalFat) {
+        this.fat = totalFat;
+    }
+    
 }
