3f8385f961d24ed27d79dd41c07ec215ed105bfb
Aleli Macapagal <amacapag@my.centennialcollege.ca>
2025-03-07 19:11:09 -0500

    Meal Plans grouped by day in collapsible style & added delete icon

--- Diet_Planner-Comp313-003/src/main/java/com/dietplanner/model/MealPlan.java
+++ Diet_Planner-Comp313-003/src/main/java/com/dietplanner/model/MealPlan.java
@@ -25,37 +25,4 @@ public class MealPlan {
     private int totalProtein;
     private int totalFat;
 
-    
-    public int getTotalCalories() {
-        return totalCalories;
-    }
-
-    public int getTotalCarbohydrate() {
-        return totalCarbohydrate;
-    }
-
-    public int getTotalProtein() {
-        return totalProtein;
-    }
-
-    public int getTotalFat() {
-        return totalFat;
-    }
-    
-    public void setTotalCalories(Integer totalCalories) {
-        this.calories = totalCalories;
-    }
-
-    public void setTotalCarbohydrate(Integer totalCarbohydrate) {
-        this.carbohydrate = totalCarbohydrate;
-    }
-
-    public void setTotalProtein(Integer totalProtein) {
-        this.protein = totalProtein;
-    }
-
-    public void setTotalFat(Integer totalFat) {
-        this.fat = totalFat;
-    }
-    
 }
