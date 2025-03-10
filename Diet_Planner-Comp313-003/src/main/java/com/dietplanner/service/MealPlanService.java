e1ff6576f15c237bf57b05702e83b1552773782b
Aleli Macapagal <amacapag@my.centennialcollege.ca>
2025-03-10 19:48:42 -0400

    Delete saved meal plan reUpdate

--- Diet_Planner-Comp313-003/src/main/java/com/dietplanner/service/MealPlanService.java
+++ Diet_Planner-Comp313-003/src/main/java/com/dietplanner/service/MealPlanService.java
@@ -1,22 +1,19 @@
 package com.dietplanner.service;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.dietplanner.model.MealPlan;
 import com.dietplanner.repository.MealPlanRepository;
-
 import jakarta.transaction.Transactional;
 
 @Service
 public class MealPlanService {
+	
     @Autowired
     private MealPlanRepository mealPlanRepository;
-
+    
     @Transactional
     public Map<String, Object> getUserMealPlans(Long accountId) {
         List<MealPlan> mealPlans = mealPlanRepository.getUserMealPlans(accountId);
@@ -26,11 +23,9 @@ public class MealPlanService {
     
     private Map<String, MealPlan> groupByAssignedDay(List<MealPlan> mealPlans) {
         Map<String, MealPlan> dailyTotals = new HashMap<>();
-
         for (MealPlan meal : mealPlans) {
             String assignedDay = meal.getAssignedDay();
             dailyTotals.putIfAbsent(assignedDay, new MealPlan());
-
             MealPlan mealPlan = dailyTotals.get(assignedDay);
             mealPlan.setTotalCalories(mealPlan.getTotalCalories() + meal.getCalories());
             mealPlan.setTotalCarbohydrate(mealPlan.getTotalCarbohydrate() + meal.getCarbohydrate());
@@ -40,5 +35,11 @@ public class MealPlanService {
 
         return dailyTotals;
     }
-    
+
+    @Transactional
+    public void deleteDailyTotal(Long userId, String assignedDay) {
+        // Delete meal plans related to the assigned day for the user.
+        mealPlanRepository.deleteMealPlansByDay(userId, assignedDay);
+    }    
+
 }
